fa41f497372c2952489e699052e0f2fedf86f1c3
yong_won <102265407+yongyaa@users.noreply.github.com>
2023-04-06 21:43:44 +0900

    Add files via upload

---
+++ home/work.js
@@ -0,0 +1,13 @@
+let a = 60
+if(a == 100 || a >= 90){
+ console.log('A')
+}else if(a == 89 || a>= 80 ){
+ console.log('B')
+}else if(a == 79 || a>= 70){
+ console.log('C')
+}else if(a == 69 || a>= 60){
+ console.log('D')
+}else{
+ console.log('F')
+}
+
